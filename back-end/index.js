49355be42dcbfc88c941b1d5569bb1855e44a98c
Y.S.V.SIDDHARDA <y.siddhu3@gmail.com>
2024-08-07 22:54:23 +0530

    minor changes

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,8 @@
 const connectToMongo = require('./db')
 const express = require('express')
 const cors = require('cors')
-connectToMongo()
-const app = express()
+await connectToMongo()
+const app = express();
 const port = 5000
 
 app.use(cors())
