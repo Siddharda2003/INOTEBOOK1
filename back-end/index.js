d405adb2295c24b1037feb5aa533887a050b6fb5
Y.S.V.SIDDHARDA <y.siddhu3@gmail.com>
2024-08-07 23:26:10 +0530

    minor changes

--- back-end/index.js
+++ back-end/index.js
@@ -6,11 +6,16 @@ const app = express();
 const port = 5000
 
 app.use(express.json())
-app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
-  });
+app.use(cors({
+    origin: 'https://inotebook-frontend-amber.vercel.app', // Specify the allowed origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Specify the allowed headers
+}));
+// app.use(cors())
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     next();
+//   });
 
 //available rouest
 app.use('/routes/auth',require('./routes/auth'))
